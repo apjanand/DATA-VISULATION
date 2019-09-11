0adc97e7a5554cea601cf6951ec6e9b4c11cdc89
apjanand <55202089+apjanand@users.noreply.github.com>
2019-09-11 21:36:07 +0530

    Add files via upload

---
+++ line.py
@@ -0,0 +1,29 @@
+#LINE CHART
+import matplotlib.pyplot as pl
+x=[1,2,3]
+y=[4,5,6]
+pl.pie(x)
+pl.show()
+
+
+
+
+
+'''import matplotlib.pyplot as plt 
+  
+# x axis values 
+x = [1,2,3] 
+# corresponding y axis values 
+y = [2,4,6]
+
+plt.xlabel("X values")
+plt.ylabel("Y values")
+  
+# plotting the points  
+plt.plot(x, y)   
+
+# giving a title to my graph 
+plt.title('My first graph!') 
+  
+# function to show the plot 
+plt.show() '''
